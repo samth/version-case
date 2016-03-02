da496dc183325d9dd3bebcdf2e2813d7ee5e87c9
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2016-03-02 11:32:54 -0500

    fix pkg deps

--- info.rkt
+++ info.rkt
@@ -4,3 +4,12 @@
 (define collection "version-case")
 (define version "1.9")
 (define scribblings '(("version-case.scrbl" ())))
+(define deps '("base"
+               "compatibility-lib"
+               "drracket-plugin-lib"
+               "gui-lib"
+               "scheme-lib"
+               "srfi-lib"
+               "srfi-lite-lib"))
+(define build-deps '("racket-doc"
+                     "scribble-lib"))
